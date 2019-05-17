ca5e9571588ed75293b95b8bd6d99943c38a69d9
manish <thankimanish35@gmail.com>
2019-05-17 17:44:02 +0530

    first commit c

--- appEvent/src/main/java/com/eventlibrary/EEvent.kt
+++ appEvent/src/main/java/com/eventlibrary/EEvent.kt
@@ -2,9 +2,11 @@ package com.eventlibrary
 
 
 class EEvent {
+
     var lable: String? = null
     var element: String? = null
     var screen_name: String? = null
+
     constructor(category: String, action: String, label: String) {
         this.lable = category
         this.element = action
