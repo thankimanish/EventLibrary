db55eabf20d945bdae5f9ee1e7d5cc2646b59bd2
manish <thankimanish35@gmail.com>
2019-05-17 18:28:05 +0530

    first commit

--- appEvent/src/main/java/com/eventlibrary/EEvent.kt
+++ appEvent/src/main/java/com/eventlibrary/EEvent.kt
@@ -6,7 +6,6 @@ class EEvent {
     var lable: String? = null
     var element: String? = null
     var screen_name: String? = null
-
     constructor(category: String, action: String, label: String) {
         this.lable = category
         this.element = action
